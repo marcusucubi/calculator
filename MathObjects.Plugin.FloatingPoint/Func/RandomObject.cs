755b05b0b519e0cb9e8ab905258389199a584203
polyfartus <polyfartus@gmail.com>
2016-04-19 17:52:32 -0500

    update

---
+++ MathObjects.Plugin.FloatingPoint/Func/RandomObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MathObjects.Framework;
+
+namespace MathObjects.Plugin.FloatingPoint
+{
+    class RandomObject : AbstractMathObject, IHasOutput, IHasDisplayValue
+    {
+        readonly double value;
+
+        public RandomObject(double value)
+        {
+            this.value = value;
+        }
+
+        public IMathObject Output
+        {
+            get { return new MathObject(value); }
+        }
+
+        public string DisplayValue 
+        { 
+            get { return "" + this.value; } 
+        }
+
+        public override string ToString()
+        {
+            return "random object(" + DisplayValue + ")";
+        }
+    }
+}
+
---
+++ MathObjects.Plugin.FloatingPoint/Func/RandomOperation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using MathObjects.Framework;
+using MathObjects.Core.DecoratableObject;
+
+namespace MathObjects.Plugin.FloatingPoint
+{
+    class RandomOperation : AbstractMathOperation
+    {
+        public override int NumberOfParameters { get { return 1; } }
+
+        public override string Symbol { get { return "random"; } }
+
+        public RandomOperation()
+        {
+        }
+      
+        public override IMathObject Perform(IMathObject[] target)
+        {
+            var input = target[0].GetDouble();
+
+            var value = new Random().Next((int) input);
+
+            var result = new RandomObject(value);
+
+            result.CopyDecorations(this);
+
+            return result;
+        }
+
+        public class Factory : AbstractMathObject, IMathOperationFactory2
+        {
+            public Factory()
+            {
+            }
+
+            public void Init(
+                IMathOperationFactoryContext context)
+            {
+            }
+
+            public IMathOperation Perform(
+                IMathOperationFactoryContext context)
+            {
+                return new RandomOperation();
+            }
+
+            public class Factory2 : IMathObjectFactory
+            {
+                public Factory2()
+                {
+                }
+
+                public IMathObject Create(
+                    IMathObjectFactoryContext context)
+                {
+                    return new RandomOperation.Factory();
+                }
+            }
+        }
+    }
+}
+
